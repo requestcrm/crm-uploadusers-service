96b76568b511f4d915a478a210811b1fe87c2c87
bandari1 <s.bandari.1@elsevier.com>
2018-10-03 14:39:10 +0100

    added accounts section

---
+++ src/main/java/com/crm/YellCRM/controller/AccountsController.java
@@ -0,0 +1,61 @@
+package com.crm.YellCRM.controller;
+
+import com.crm.YellCRM.database.Accounts;
+import com.crm.YellCRM.repository.AccountsRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+public class AccountsController {
+
+    private static final Logger LOGGER =  LoggerFactory.getLogger(AccountsController.class);
+
+    private AccountsRepository accountsRepository;
+
+    @Autowired
+    public AccountsController(AccountsRepository accountsRepository){
+        this.accountsRepository = accountsRepository;
+    }
+
+    //This is to get company from UJ-1 and insert in to accounts table which can be used by UJ-2
+    @RequestMapping(value = "/{company}", method = RequestMethod.GET)
+    public ResponseEntity getResult(@PathVariable("company") String company){
+        LOGGER.info("Entry in to GET Controller - for inserting in to Accounts ");
+        try {
+            Accounts acc = new Accounts();
+            acc.setId(UUID.randomUUID().toString());
+            acc.setName(company);
+            acc.setBillingAddressPostcode("OX5 1GB");
+            acc.setAssignedUserId("1");
+            accountsRepository.save(acc);
+        }
+        catch(Exception ex){
+            LOGGER.error("Failed to insert the company in to accounts for: " + company , ex);
+            return new ResponseEntity(ex.getMessage(), HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok("Record Inserted in to accounts: " + company);    }
+
+
+    public void insertRecord(String company) {
+        LOGGER.info("Entry in to Accounts Controller - for inserting in to Accounts ");
+        try {
+            Accounts acc = new Accounts();
+            acc.setId(UUID.randomUUID().toString());
+            acc.setName(company);
+            acc.setBillingAddressPostcode("OX5 1GB");
+            acc.setAssignedUserId("1");
+            accountsRepository.save(acc);
+        } catch (Exception ex) {
+            LOGGER.error("Failed to insert the company in to accounts for: " + company, ex);
+        }
+    }
+}
